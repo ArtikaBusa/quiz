9fb04cf052e1bdc1d9da8a92755b64de40cfdf13
Artika Busa <busaartika@gmail.com>
2025-12-19 16:05:45 +0530

    adding public page

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,52 @@
 class QuizzesController < ApplicationController
+  before_action :set_quiz, only: [:show, :submit]
+
   def index
+    @quizzes = Quiz.all
   end
 
   def show
+    if params[:submission_id].present?
+      @submission = @quiz.submissions.includes(submission_answers: :question).find_by(id: params[:submission_id])
+    end
+  end
+
+  def submit
+    answers = params[:answers] || {}
+
+    score = 0
+
+    submission = @quiz.submissions.create!
+
+    answers.each do |question_id, user_answer|
+      question = @quiz.questions.find(question_id)
+
+      correct = case question.question_type
+      when "mcq"
+        question.mcq_options.where(correct: true).pluck(:text).include?(user_answer)
+      when "boolean"
+        question.correct_boolean_answer.to_s == user_answer
+      when "text"
+        question.correct_text_answer.to_s.strip.downcase ==
+          user_answer.to_s.strip.downcase
+      end
+
+      score += 1 if correct
+
+      submission.submission_answers.create!(
+        question: question,
+        answer: user_answer
+      )
+    end
+
+    submission.update!(score: score)
+
+    redirect_to quiz_path(@quiz, submission_id: submission.id), notice: "Quiz submitted! Your score: #{score}"
+  end
+
+  private
+
+  def set_quiz
+    @quiz = Quiz.includes(:questions).find_by(id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
   
-  resources :quizzes, only: [:index, :show]
+  resources :quizzes, only: [:index, :show] do
+    post :submit, on: :member
+  end
   root "quizzes#index"
 end
